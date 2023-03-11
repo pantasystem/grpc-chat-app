cb1527c9b6c94966c0f76ba696a36c3f3de4df88
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-03-11 14:32:38 +0900

    feat: MessageにアクセスするためのRepositoryを作成

---
+++ client/lib/repositories/message_repository.dart
@@ -0,0 +1,35 @@
+import 'package:client/generated/proto/message.pbgrpc.dart';
+import 'package:client/repositories/auth_repository.dart';
+import 'package:grpc/grpc.dart';
+
+class MessageRepository {
+  MessageRepository({required this.client, required this.authRepository});
+
+  final MessageServiceClient client;
+  final AuthRepository authRepository;
+
+  Future<List<Message>> findMessages({required String roomId}) async {
+    final res = await client.findAllMessages(
+      FindAllMessagesRequest(roomId: roomId),
+      options: CallOptions(
+        metadata: {
+          "Authorization": "Bearer ${await authRepository.getToken()}"
+        },
+      ),
+    );
+    return res.messages;
+  }
+
+  Future<Message> create({required String text}) async {
+    return await client.createMessage(
+      CreateMessageRequest(
+      text: text,
+      ),
+      options: CallOptions(
+        metadata: {
+          "Authorization": "Bearer ${await authRepository.getToken()}"
+        },
+      ),
+    );
+  }
+}
